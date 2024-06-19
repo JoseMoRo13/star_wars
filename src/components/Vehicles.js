d8665e8f014159dc5b67066a4587e8e8f2f21016
JoseMoRo13 <josemontero93@gmail.com>
2024-06-19 14:10:31 -0600

    4

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -28,22 +28,34 @@ const ListItem = styled.li`
   text-align: center;
 `;
 
-const Planets = () => {
-  const { data: planets, loading, error } = useFetch('https://swapi.dev/api/planets/');
+const Vehicles = () => {
+  const { data: vehicles, loading, error } = useFetch('https://swapi.dev/api/vehicles/');
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error loading data</p>;
 
   return (
     <Container>
-      <Title>Planets</Title>
+      <Title>Veiculos</Title>
       <List>
-        {planets.map(planet => (
-          <ListItem key={planet.name}>{planet.name}</ListItem>
+        {vehicles.map(vehicles => (
+          <ListItem>
+            <p>Nombre: {vehicles.name}</p>
+            <p>Modelo: {vehicles.model}</p>
+            <p>Manufactura: {vehicles.manufacturer}</p>
+            <p>Costo: {vehicles.cost_in_credits}</p>
+            <p>Tamaño: {vehicles.length}</p>
+            <p>Velocidad Atmosfera: {vehicles.max_atmosphering_speed}</p>
+            <p>Tripulación: {vehicles.crew}</p>
+            <p>Pasageros: {vehicles.passengers}</p>
+            <p>Capacidad de carga: {vehicles.cargo_capacity}</p>
+            <p>Alimentación: {vehicles.consumables}</p>
+            <p>Clase de veículo: {vehicles.vehicle_class}</p>
+          </ListItem>
         ))}
       </List>
     </Container>
   );
 };
 
-export default Planets;
+export default Vehicles;
