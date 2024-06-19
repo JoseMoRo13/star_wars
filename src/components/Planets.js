bf499bd50669a95e183cdd93f0e075df56ab63c5
JoseMoRo13 <josemontero93@gmail.com>
2024-06-19 06:24:15 -0600

    1

---
+++ src/components/Planets.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+import useFetch from '../useFetch';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px;
+`;
+
+const Title = styled.h1`
+  font-family: 'Arial', sans-serif;
+  color: #333;
+`;
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const ListItem = styled.li`
+  background: #f4f4f4;
+  padding: 10px;
+  margin: 5px 0;
+  border-radius: 4px;
+  width: 300px;
+  text-align: center;
+`;
+
+const Planets = () => {
+  const { data: planets, loading, error } = useFetch('https://swapi.dev/api/planets/');
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error loading data</p>;
+
+  return (
+    <Container>
+      <Title>Planets</Title>
+      <List>
+        {planets.map(planet => (
+          <ListItem>
+            <p>Nombre: {planet.name}{planet.name}</p>
+          </ListItem>
+        ))}
+      </List>
+    </Container>
+  );
+};
+
+export default Planets;
